e6ab8e1ddd4d034fbdeb3649275ebd18cba20bb2
Acer54 <matthias.laumer@web.de>
2016-12-07 19:50:33 +0100

    German translation

--- locale/local_de.ts
+++ locale/local_de.ts
@@ -4,89 +4,114 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../noteshrinker-qt.py" line="194"/>
+        <location filename="../noteshrinker-qt.py" line="272"/>
         <source>&amp;Exit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="194"/>
+        <location filename="../noteshrinker-qt.py" line="272"/>
         <source>Exit the Application</source>
         <translation>Beendet die Anwendung</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="216"/>
+        <location filename="../noteshrinker-qt.py" line="283"/>
         <source>&amp;Remove position(s)</source>
         <translation>Position(en) &amp;Entfernen</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="200"/>
+        <location filename="../noteshrinker-qt.py" line="283"/>
         <source>Removes the selected position(s)</source>
         <translation>Entfernt die markierte Position</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="211"/>
+        <location filename="../noteshrinker-qt.py" line="294"/>
         <source>&amp;Move Up</source>
         <translation>Nach &amp;oben</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="211"/>
+        <location filename="../noteshrinker-qt.py" line="294"/>
         <source>Move the selected position UP</source>
         <translation>Bewegt die markierte Position nach OBEN</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="216"/>
+        <location filename="../noteshrinker-qt.py" line="299"/>
         <source>Move the selected position DOWN</source>
         <translation>Bewegt die markierte Position nach UNTEN</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="227"/>
+        <location filename="../noteshrinker-qt.py" line="310"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="231"/>
+        <location filename="../noteshrinker-qt.py" line="315"/>
         <source>&amp;Edit</source>
         <translation>&amp;Bearbeiten</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="237"/>
+        <location filename="../noteshrinker-qt.py" line="321"/>
         <source>&amp;Extras</source>
         <translation>&amp;Extras</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="241"/>
+        <location filename="../noteshrinker-qt.py" line="325"/>
         <source>&amp;Help</source>
         <translation>&amp;Hilfe</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="175"/>
+        <location filename="../noteshrinker-qt.py" line="253"/>
         <source>Preview</source>
         <translation>Vorschau</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="175"/>
+        <location filename="../noteshrinker-qt.py" line="253"/>
         <source>Filename</source>
         <translation>Dateiname</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="175"/>
+        <location filename="../noteshrinker-qt.py" line="253"/>
         <source>Size</source>
         <translation>Größe</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="206"/>
+        <location filename="../noteshrinker-qt.py" line="289"/>
         <source>&amp;Add position(s)</source>
         <translation>Position(en) &amp;hinzufügen</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="206"/>
+        <location filename="../noteshrinker-qt.py" line="289"/>
         <source>Adds the selected position(s)</source>
         <translation>Fügt die ausgewählte(n) Position(en) zur Übersicht hinzu</translation>
     </message>
     <message>
-        <location filename="../noteshrinker-qt.py" line="414"/>
+        <location filename="../noteshrinker-qt.py" line="825"/>
         <source>Finished!</source>
         <translation>Fertig!</translation>
+    </message>
+    <message>
+        <location filename="../noteshrinker-qt.py" line="278"/>
+        <source>&amp;Add Folder</source>
+        <translation>aus Ordner hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../noteshrinker-qt.py" line="278"/>
+        <source>Select a Folder with Images to be added</source>
+        <translation>WÄhle einen Ordner aus, aus dem Bilder  hinzugefügt werden sollen</translation>
+    </message>
+    <message>
+        <location filename="../noteshrinker-qt.py" line="299"/>
+        <source>&amp;RMove Down</source>
+        <translation>Nach unten verschieben</translation>
+    </message>
+    <message>
+        <location filename="../noteshrinker-qt.py" line="515"/>
+        <source>Please select a folder to be added:</source>
+        <translation>Bitte wähle einen Ordner der hinzugefügt werden soll</translation>
+    </message>
+    <message>
+        <location filename="../noteshrinker-qt.py" line="671"/>
+        <source>Please select a folder where to save output:</source>
+        <translation>Bitte wäle einen Ordner in dem die Ausgabe gespeichert wird</translation>
     </message>
 </context>
 <context>
@@ -123,18 +148,18 @@
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="119"/>
-        <source>create single PDF</source>
+        <source>create single PDFs</source>
         <translation>erstelle einzelne PDF</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="128"/>
         <source>Generate!</source>
-        <translation>Generiere!</translation>
+        <translation type="obsolete">Generiere!</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="182"/>
         <source>Filename:</source>
-        <translation>Dateiname:</translation>
+        <translation type="obsolete">Dateiname:</translation>
     </message>
     <message>
         <location filename="../ui/mainwindow.ui" line="226"/>
@@ -166,11 +191,21 @@
         <source>Apply Settings to all Images</source>
         <translation>Für alle Bilder übernehmen</translation>
     </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="134"/>
+        <source>Go!</source>
+        <translation>Los!</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="178"/>
+        <source>Filename-Extention:</source>
+        <translation>Dateinamen-Erweiterung:</translation>
+    </message>
 </context>
 <context>
     <name>Workbench_Tablewidget</name>
     <message>
-        <location filename="../lib/Workbench.py" line="115"/>
+        <location filename="../lib/Workbench.py" line="243"/>
         <source>Remove</source>
         <translation>Entfernen</translation>
     </message>
